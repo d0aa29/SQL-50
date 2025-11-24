96f59865228e3186adc74576cfda8e3e9cfdf750
Doaa Zakaria <117455187+d0aa29@users.noreply.github.com>
2025-11-24 15:57:46 +0200

    Create Immediate Food Delivery II.sql

---
+++ Basic Aggregate Functions/MySQL/Immediate Food Delivery II.sql
@@ -0,0 +1,13 @@
+
+select Round(
+  COALESCE( 
+  sum(CASE WHEN d.order_date=d.customer_pref_delivery_date  THEN 1 ELSE 0 END)/count(d.order_date)*100
+    ,0)
+  ,2)
+  as immediate_percentage 
+from Delivery d
+join( select customer_id ,min(order_date) as first_date 
+      from Delivery 
+     group by customer_id )f 
+ON d.customer_id = f.customer_id
+AND d.order_date = f.first_date

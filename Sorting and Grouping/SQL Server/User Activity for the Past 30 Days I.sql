ce85ddd59d6b4c74066795a24f98df6ceaf3999c
Doaa Zakaria <117455187+d0aa29@users.noreply.github.com>
2025-11-25 14:35:23 +0200

    Create User Activity for the Past 30 Days I.sql

---
+++ Sorting and Grouping/SQL Server/User Activity for the Past 30 Days I.sql
@@ -0,0 +1,16 @@
+/*
+NOTE: The original problem statement can feel unclear about what exactly should be counted although its easy.
+so their is explanation if u feel lost in the problem description
+
+The question asks us to return, for each day in the 30-day period ending on 2019-07-27
+(meaning we only consider activities between '2019-06-28' and '2019-07-27'), the number of users who were active on that day.
+
+=> A user is considered active if they performed ANY activity on that date.
+=> Each user is counted only once per day, no matter how many activities they performed.
+=> But if the same user is active again on another day, they are counted again for that day.
+*/
+
+select activity_date as day, count( distinct user_id) as active_users 
+from Activity 
+where activity_date between '2019-06-28' AND '2019-07-27'
+group by activity_date 

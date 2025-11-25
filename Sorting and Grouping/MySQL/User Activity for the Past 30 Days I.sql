735979a60a72925a4e8f4606ee402e6b429c1697
Doaa Zakaria <117455187+d0aa29@users.noreply.github.com>
2025-11-25 14:36:57 +0200

    Update User Activity for the Past 30 Days I.sql

--- Sorting and Grouping/MySQL/User Activity for the Past 30 Days I.sql
+++ Sorting and Grouping/MySQL/User Activity for the Past 30 Days I.sql
@@ -1,9 +1,11 @@
 /*
-NOTE: The original problem statement can feel unclear about what exactly should be counted although its easy.
-so their is explanation if u feel lost in the problem description
+NOTE: The original problem statement can feel unclear about what exactly 
+should be counted, even though the logic is simple.
+So here is an explanation in case you feel lost in the description:
 
 The question asks us to return, for each day in the 30-day period ending on 2019-07-27
-(meaning we only consider activities between '2019-06-28' and '2019-07-27'), the number of users who were active on that day.
+(meaning we only consider activities between '2019-06-28' and '2019-07-27'),
+the number of users who were active on that day.
 
 => A user is considered active if they performed ANY activity on that date.
 => Each user is counted only once per day, no matter how many activities they performed.

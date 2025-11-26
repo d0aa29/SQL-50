156219f7e4fd3765da6df4853c831911c4b6cb81
Doaa Zakaria <doaazakaria29@gmail.com>
2025-11-26 22:51:15 +0200

    Create Average Selling Price.sql

---
+++ Basic Aggregate Functions/SQL Server/Average Selling Price.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+select p.product_id ,  COALESCE(
+    ROUND(
+      SUM(1.0 * p.price * u.units) / SUM(u.units)
+    , 2)
+  , 0) as average_price 
+from Prices p left join UnitsSold u
+on p.product_id = u.product_id 
+and u.purchase_date between p.start_date and p.end_date
+group by p.product_id
